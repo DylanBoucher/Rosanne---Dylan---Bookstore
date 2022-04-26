1843422f31be34b96aef5e0d6950b59c52b166d1
DylanBoucher <dylanboucher99@gmail.com>
2022-04-26 08:28:34 -0500

    Set up server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+//Imports
+const express = require('express')
+const methodOverride = require('method-override')
+const controllers = require('./controllers')
+
+//Create instance
+const app = express()
+
+//db connection
+require('./config/db.connection')
+
+const PORT = process.env.PORT
+
+//App configs
+app.set('view engine', 'ejs')
+
+//Middleware
+app.use(express.static('public'))
+
+app.use(methodOverride('_method'))
+
+app.use(express.urlencoded({ extended: false }))
+
+//Controllers
+app.use('/products', controllers.products)
+
+//Home route
+// app.get('/', (req, res) => res.send('Home page'))
+
+//Listen
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
