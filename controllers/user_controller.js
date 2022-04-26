bb16e24f2396b5abfd8ffc27e0478d22c5a41286
rosanne-a <anderson.rosanne4@gmail.com>
2022-04-26 16:36:42 -0400

    showUser psudocode

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -5,30 +5,13 @@ const router = express.Router()
 const db = require('../models')
 
 //Show route
-router.get('/:id', async (req, res, next) => {
+router.get('/userName', async (req, res, next) => {
     try{
-        const foundBook = await db.Product.findById(req.params.id)
+        const user = await db.User.findById(req.params.id)
         const context = {
-            oneBook: foundBook,
+            users: user,
         }
-        return res.render('show.ejs', context)
-    }catch (error) {
-        console.log(error)
-        req.error = error
-        return next()
-    }
-})
-
-//Edit route
-router.get('/:id/edit', async (req, res, next) => {
-    res.send('Edit page')
-})
-
-//Create route
-router.post('/', async (req, res, next) => {
-    try{
-        const newUser = await db.User.create(req.body)
-        return res.redirect('/newUser')
+        return res.render('showUser.ejs', context)
     }catch (error) {
         console.log(error)
         req.error = error
