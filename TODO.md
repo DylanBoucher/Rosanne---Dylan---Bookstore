f566fdec76840cabb9f4e59a45017b4adefa7cd2
rosanne-a <anderson.rosanne4@gmail.com>
2022-04-26 20:11:34 -0400

    userLogin, showUser ejs complete

---
+++ TODO.md
@@ -0,0 +1,4 @@
+Reviews - Need to add a reviews controller, model, and view seperate from user
+    Need to add to user after these are created
+
+Authentication- need to add authentication to login
---
+++ models/Users.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: [true, "Username cannot be empty"],
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: [true, "Email Address cannot be empty."],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Password cannot be empty."]
+    },    
+    image: {
+        type: String,
+        required: [true, "Must have user image."],
+    },
+    booksSold: {
+        type: Number,
+        min: [0, 'You cannot have negative sales.'],
+        required: [true, "Books sold cannot be empty."]
+    },
+    location: {
+        type: String,
+    },
+}, {timestamps: true})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User;
